d98c9df7414eae43cedbc828f91580debd287cde
amalvivek <avivek99@gmail.com>
2024-03-03 13:15:41 +0530

    add vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from 'tailwindcss'
 import vitePluginSingleSpa from 'vite-plugin-single-spa'
-import { configDefaults } from 'vitest/config'
+import vercel from 'vite-plugin-vercel'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +13,8 @@ export default defineConfig({
     vitePluginSingleSpa({
       type: 'mife',
       serverPort: 3003
-    })
+    }),
+    vercel()
   ],
   resolve: {
     alias: {
