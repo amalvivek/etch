05b1e97a30ab2be4d7a0274fe2cd2940ce1a3fd7
amalvivek <avivek99@gmail.com>
2024-03-03 15:26:39 +0530

    update base

--- vite.config.ts
+++ vite.config.ts
@@ -9,27 +9,31 @@ import vercel from 'vite-plugin-vercel'
 const plugins: any[] = [tailwindcss]
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vitePluginSingleSpa({
-      type: 'mife',
-      serverPort: 3003
-    }),
-    vercel()
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  css: {
-    postcss: {
-      plugins
-    }
-  },
-  server: {
-    hmr: false
-  },
-  base: 'http://localhost:3003'
+export default defineConfig(({ command }) => {
+  let base = 'http://localhost:3003'
+  if (command === 'build') base = 'https://etch-sable.vercel.app'
+  return {
+    plugins: [
+      vue(),
+      vitePluginSingleSpa({
+        type: 'mife',
+        serverPort: 3003
+      }),
+      vercel()
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    css: {
+      postcss: {
+        plugins
+      }
+    },
+    server: {
+      hmr: false
+    },
+    base: base
+  }
 })
